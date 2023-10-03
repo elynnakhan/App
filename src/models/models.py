712489d348f4d9dd670df53c62d8694ac719b97b
AlinaKhan98 <Alina.Khan@marketplug.com>
2023-10-03 23:34:43 +0500

    first commit

--- src/models/models.py
+++ src/models/models.py
@@ -11,9 +11,9 @@ class Products(db.Base):
     ProductID = Column(Integer, primary_key=True, index=True)
     ProductName = Column(String(255))
     ProductDescription = Column(String(255))
-    ProductCategoryID = Column(Integer, ForeignKey('ProductCategory.CategoryID'))  # Establish the ProductCategory relationship
+    ProductCategoryID = Column(Integer, ForeignKey('ProductCategory.CategoryID'))  
     ProductPrice = Column(Float)
-    ManufacturerID = Column(Integer, ForeignKey('Manufacturers.ManufacturerID'))  # Establish the Manufacturer relationship
+    ManufacturerID = Column(Integer, ForeignKey('Manufacturers.ManufacturerID'))  
     CurrentInventoryQuantity = Column(Integer)
     LowStockThreshold = Column(Integer)
     DateAddedToInventory = Column(DateTime)
@@ -29,15 +29,14 @@ class Sales(db.Base):
 
     SalesID = Column(Integer, primary_key=True, index=True)
     SaleDateTime = Column(DateTime)
-    CustomerID = Column(Integer, ForeignKey('Customers.CustomerID'))  # Establish the Customer relationship
-    SalespersonID = Column(Integer, ForeignKey('Users.UserID'))  # Establish the Salesperson relationship
-    ProductID = Column(Integer, ForeignKey('Products.ProductID'))  # Establish the Product relationship
+    CustomerID = Column(Integer, ForeignKey('Customers.CustomerID'))  
+    SalespersonID = Column(Integer, ForeignKey('Users.UserID')) 
+    ProductID = Column(Integer, ForeignKey('Products.ProductID'))
     QuantitySold = Column(Integer)
     TotalRevenue = Column(Float)
     PaymentMethod = Column(String(255))
     SalesRegion = Column(String(255))
 
-    # Define the relationships
     customer = relationship("Customers", back_populates="sales")
     salesperson = relationship("Users", back_populates="sales")
     product = relationship("Products", back_populates="sales_transaction_details")
@@ -46,15 +45,14 @@ class InventoryRecords(db.Base):
     __tablename__ = "Inventory"
 
     RecordID = Column(Integer, primary_key=True, index=True)
-    ProductID = Column(Integer, ForeignKey('Products.ProductID'))  # Establish the Product relationship
+    ProductID = Column(Integer, ForeignKey('Products.ProductID'))  
     InventoryChangeType = Column(String(255))
     QuantityChange = Column(Integer)
     CurrentInventoryQuantity = Column(Integer)
     InventoryUpdateDateTime = Column(String(255))
-    PersonResponsibleID = Column(Integer, ForeignKey('Users.UserID'))  # Establish the PersonResponsible relationship
+    PersonResponsibleID = Column(Integer, ForeignKey('Users.UserID'))  
     Notes = Column(String(255))
 
-    # Define the relationships
     product = relationship("Products", back_populates="inventory_records")
     person_responsible = relationship("Users", back_populates="inventory_records")
 
@@ -66,7 +64,6 @@ class Users(db.Base):
     PasswordHash = Column(String(255))
     UserRole = Column(String(255))
 
-    # Define the reverse relationship
     sales = relationship("Sales", back_populates="salesperson")
     inventory_records = relationship("InventoryRecords", back_populates="person_responsible")
 
@@ -77,7 +74,6 @@ class ProductCategory(db.Base):
     CategoryName = Column(String(255))
     CategoryDescription = Column(String(255))
 
-    # Define the reverse relationship
     products = relationship("Products", back_populates="category")
 
 class Manufacturer(db.Base):
@@ -87,7 +83,6 @@ class Manufacturer(db.Base):
     ManufacturerName = Column(String(255))
     ManufacturerDescription = Column(String(255))
 
-    # Define the reverse relationship
     products = relationship("Products", back_populates="manufacturer")
 
 class Customers(db.Base):
@@ -98,6 +93,5 @@ class Customers(db.Base):
     CustomerContactInfo = Column(String(255))
     CustomerAddress = Column(String(255))
 
-    # Define the reverse relationship
     sales = relationship("Sales", back_populates="customer")
 
